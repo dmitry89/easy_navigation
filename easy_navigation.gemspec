31a4670b27df5dfd14683d623a28ca1dfdd7e0c8
Ivan Torres <ivan@innetra.com>
2009-04-25 14:48:07 -0500

    making 118n optional

--- easy_navigation.gemspec
+++ easy_navigation.gemspec
@@ -2,11 +2,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{easy_navigation}
-  s.version = "2.0.0"
+  s.version = "2.1.0"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 1.2") if s.respond_to? :required_rubygems_version=
   s.authors = ["Ivan Torres"]
-  s.date = %q{2008-12-21}
+  s.date = %q{2009-04-25}
   s.description = %q{Easy navigation for Ruby on Rails 2.2 (i18n)}
   s.email = %q{mexpolk@gmail.com}
   s.extra_rdoc_files = ["README.rdoc", "lib/easy_navigation.rb"]
@@ -16,12 +16,12 @@ Gem::Specification.new do |s|
   s.rdoc_options = ["--line-numbers", "--inline-source", "--title", "Easy_navigation", "--main", "README.rdoc"]
   s.require_paths = ["lib"]
   s.rubyforge_project = %q{easy_navigation}
-  s.rubygems_version = %q{1.3.1}
+  s.rubygems_version = %q{1.3.2}
   s.summary = %q{Easy navigation for Ruby on Rails 2.2 (i18n)}
 
   if s.respond_to? :specification_version then
     current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
-    s.specification_version = 2
+    s.specification_version = 3
 
     if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
     else
--- lib/easy_navigation.rb
+++ lib/easy_navigation.rb
@@ -1,7 +1,7 @@
 module EasyNavigation
 
   module Helper
-  
+
     def easy_navigation(name, options = {})
 
       navigation_class_name = (options[:navigation_class] || "navigation").to_s
@@ -11,7 +11,7 @@ module EasyNavigation
       separator = options[:separator] || false
 
       tabs_html = ""
-      
+
       EasyNavigation::Builder.navigation[name][:tabs].each do |tab|
         menus_html = ""
         current_tab = false
@@ -19,45 +19,45 @@ module EasyNavigation
           if current = current_menu?(menu)
             current_tab = true
           end
-          menus_html << self.render_menu(menu[:name], menu[:text], 
-            menu[:url].merge!(:skip_relative_url_root => true), 
+          menus_html << self.render_menu(menu[:name], menu[:text],
+            menu[:url].merge!(:skip_relative_url_root => true),
             ("#{menu_class_name} current" if current) || menu_class_name)
           if separator
            menus_html << self.render_separator(separator_class_name)
           end
         end
-        tabs_html << self.render_tab(tab[:name], 
-          tab[:text], 
-          tab[:url].merge!(:skip_relative_url_root => true), 
+        tabs_html << self.render_tab(tab[:name],
+          tab[:text],
+          tab[:url].merge!(:skip_relative_url_root => true),
           menus_html, ("#{tab_class_name} current" if current_tab) || tab_class_name)
       end
       self.render_navigation("navigation_" << name.to_s, tabs_html, navigation_class_name)
     end
-    
+
     protected
-    
+
     def render_separator(class_name)
       content_tag("li", "|", :class => class_name)
     end
-    
+
     def render_menu(id, text, url, class_name)
-      content_tag("li", link_to(t(text), url), :id => id, :class => class_name)
+      content_tag("li", link_to(text, url), :id => id, :class => class_name)
     end
-    
+
     def render_tab(id, text, url, menus_html, class_name)
       content_tag("li",
-        "#{link_to(t(text), url)} #{content_tag("ul", menus_html)}",
+        "#{link_to(text, url)} #{content_tag("ul", menus_html)}",
         :id => id, :class => class_name)
     end
-    
+
     def render_navigation(id, tabs_html, class_name)
       content_tag("ul", tabs_html, :id => id, :class => class_name)
     end
-    
+
     def current_menu?(menu)
-      current = controller.params[:controller] == menu[:url][:controller].gsub(/^\//, "") && 
+      current = controller.params[:controller] == menu[:url][:controller].gsub(/^\//, "") &&
         (controller.action_name == menu[:url][:action] || menu[:url][:action] == nil)
-      if menu.has_key?:on 
+      if menu.has_key?:on
          (menu[:on].is_a?(Array) ? menu[:on] : [menu[:on]]).each do |controllers|
           (controllers.is_a?(Array) ? controllers : [controllers]).each do |c|
              current |= controller.params[:controller] == c[:controller].gsub(/^\//, "")
@@ -68,7 +68,7 @@ module EasyNavigation
               current &= !((c[:except].is_a?(Array) ? c[:except] : [c[:except]]).include?controller.action_name)
             end
           end
-        end      
+        end
       end
       current
     end
@@ -78,100 +78,105 @@ module EasyNavigation
   class Configuration
 
     attr_accessor :navigation
-    
+
     def initialize
       self.navigation = {}
     end
-    
+
     def config(&block)
       builder = Builder.new
       yield builder
-      builder.navigations.each { |tmp| 
+      builder.navigations.each { |tmp|
         self.navigation[tmp[:name]] = tmp
       }
     end
-    
+
     class Builder
-      
+
       attr_accessor :navigations
-    
+
       def initialize
         self.navigations = []
       end
-        
+
       def navigation(name, options = {}, &block)
         navigation = Navigation.new(name, options.merge!(:prefix => "navigation"))
         yield navigation
         self.navigations << navigation.build
       end
-      
+
       def build
         { :navigations => navigations }
       end
-      
+
       class Navigation
-      
+
         attr_accessor :tabs, :name, :options
-      
+
         def initialize(name, options = {})
           self.tabs = []
           self.name = name
           self.options = options
         end
-      
+
         def tab(name, options = {}, &block)
+          options.merge!(self.options)
           tab = Tab.new(name, options.merge!(:prefix => [self.options[:prefix], self.name]))
           yield tab
           self.tabs << tab.build
         end
-        
+
         def build
           self.options[:separator] = false unless self.options.has_key?(:separator)
           { :name => self.name.to_sym, :tabs => self.tabs, :options => self.options }
         end
-        
+
         class Tab
-        
-          attr_accessor :menus, :name, :options
-          
+
+          attr_accessor :menus, :name, :options, :text
+
           def initialize(name, options = {})
             self.menus = []
             self.name = name
             self.options = options
+            self.text = name.to_s.gsub(/_/, " ").gsub(/\b('?[a-z])/) { $1.capitalize }
+            self.text = I18n.t([self.options[:prefix], self.name, "title"].join(".")) if self.options[:i18n] == true
           end
-          
+
           def menu(name, options = {}, &block)
+            options.merge!(self.options)
             menu = Menu.new(name, options.merge!(:prefix => [self.options[:prefix], self.name, "menus"]))
             yield menu
             self.menus << menu.build
           end
-          
+
           def build
-            { :name => [self.options[:prefix], self.name].join("_").to_sym, 
-              :text => [self.options[:prefix], self.name, "title"].join("."), 
+            { :name => [self.options[:prefix], self.name].join("_").to_sym,
+              :text => self.text,
               :url => self.options[:url],
-              :options => self.options, 
+              :options => self.options,
               :menus => self.menus }
           end
-          
+
           class Menu
-            
-            attr_accessor :name, :options, :active_urls
-            
-            def initialize(name, params = {})
+
+            attr_accessor :active_urls, :name, :options, :text
+
+            def initialize(name, options = {})
               self.active_urls = []
               self.name = name
-              self.options = params
+              self.options = options
+              self.text = self.name.to_s.gsub(/_/, " ").gsub(/\b('?[a-z])/) { $1.capitalize }
+              self.text = I18n.t([self.options[:prefix], self.name, "title"].join(".")) if self.options[:i18n] == true
             end
-            
+
             def build
-              self.options[:prefix] << self.name
-              { :name => self.options[:prefix].join("_").to_sym, 
-                :text => self.options[:prefix].join("."), 
-                :url => self.options[:url], 
+              { :name => self.options[:prefix].join("_").to_sym,
+                :text => text,
+                :url => self.options[:url],
                 :on => self.active_urls }
             end
-            
+
             def connect(options = {})
               options[:controller] = self.options[:url][:controller] unless options.has_key?(:controller)
               self.active_urls << options
@@ -182,9 +187,9 @@ module EasyNavigation
       end # Navigation
     end # Builder
   end # Configuration
-  
+
   Builder = Configuration.new
-  
+
 end # EasyNavigation
 
 ActionView::Base.send :include, EasyNavigation::Helper
